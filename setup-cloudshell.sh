f0ce90f04a5db8685f8754c2c52f8fc045671073
Taylor Silva <tsilva@prophix.com>
2022-11-10 14:24:22 -0500

    more robustness
    Signed-off-by: Taylor Silva <tsilva@prophix.com>

--- setup-cloudshell.sh
+++ setup-cloudshell.sh
@@ -15,17 +15,19 @@ if [[ ! -e $(which op) ]]; then
     rm 1pass.zip op.sig
 fi
 
-echo "NOTE: You are about to sign into 1Password. It will first prompt you for the address to log into."
-echo "Use the following signin address: prophix-it.1password.com"
-echo "Press ENTER to continue..."
-read
-eval $(op signin --account prophix-it.1password.com)
-
 # get ssh key from 1pass
 if [[ ! -e ~/.ssh/private_key ]]; then
+    echo "NOTE: You are about to sign into 1Password. It will first prompt you for the address to log into."
+    echo "Use the following signin address: prophix-it.1password.com"
+    echo "Press ENTER to continue..."
+    read
+    eval $(op signin --account prophix-it.1password.com)
+
     mkdir -p ~/.ssh
     op item get ssh-key --vault Private --field notesPlain --format json | jq -r '.value' > ~/.ssh/private_key
     chmod 600 ~/.ssh/private_key
+else
+    echo "ssh key already downloaded"
 fi
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/private_key
@@ -33,13 +35,21 @@ ssh-add ~/.ssh/private_key
 # clone other repo's: cloud-ops, infrastructure
 if [[ ! -d ~/cloud-ops ]]; then
     git clone git@github.com:prophix-cloud/cloud-ops.git
+else
+    echo "cloud-ops repo already cloned"
 fi
 
 if [[ ! -d ~/infrastructure ]]; then
     git clone git@github.com:prophix-cloud/infrastructure.git
+else
+    echo "infrastructure repo already cloned"
 fi
 
 
 #install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [[ ! -e ~/.vim/autoload/plug.vim ]]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+    echo "vim-plug already installed"
+fi
