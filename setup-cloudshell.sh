222db545322d0c04f548e6ad3f13e9e2d217f378
Taylor Silva <tsilva@prophix.com>
2022-11-24 16:17:37 -0500

    move to local bin folder
    then sudo is not required
    
    Signed-off-by: Taylor Silva <tsilva@prophix.com>

--- setup-cloudshell.sh
+++ setup-cloudshell.sh
@@ -30,7 +30,7 @@ if [[ ! -e $(which terraformer) ]]; then
     export PROVIDER={aws,datadog}
     curl -LO https://github.com/GoogleCloudPlatform/terraformer/releases/download/$(curl -s https://api.github.com/repos/GoogleCloudPlatform/terraformer/releases/latest | grep tag_name | cut -d '"' -f 4)/terraformer-${PROVIDER}-linux-amd64
     chmod +x terraformer-${PROVIDER}-linux-amd64
-    mv terraformer-${PROVIDER}-linux-amd64 /usr/local/bin/terraformer
+    mv ~/bin/terraformer-${PROVIDER}-linux-amd64 /usr/local/bin/terraformer
 fi
 
 # get ssh key from 1pass
