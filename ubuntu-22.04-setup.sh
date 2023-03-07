065cfc7065d1ae3d682a971dfa7bd1f572af2450
Taylor Silva <dev@taydev.net>
2023-03-07 15:11:19 -0500

    insall the all provider binary
    previous version only installed datadog
    
    Signed-off-by: Taylor Silva <dev@taydev.net>

--- ubuntu-22.04-setup.sh
+++ ubuntu-22.04-setup.sh
@@ -35,7 +35,7 @@ fi
 
 # Install terraformer https://github.com/GoogleCloudPlatform/terraformer
 if [[ ! -e $(which terraformer) ]]; then
-    export PROVIDER={aws,datadog}
+    export PROVIDER=all
     curl -LO https://github.com/GoogleCloudPlatform/terraformer/releases/download/$(curl -s https://api.github.com/repos/GoogleCloudPlatform/terraformer/releases/latest | grep tag_name | cut -d '"' -f 4)/terraformer-${PROVIDER}-linux-amd64
     chmod +x terraformer-${PROVIDER}-linux-amd64
     mv terraformer-${PROVIDER}-linux-amd64 ~/bin/terraformer
